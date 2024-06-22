f5af74d05453c979af34984ffd1ab4a32ac20819
istvzsig <istvanzsigmond@aol.com>
2024-06-22 23:24:06 +0200

    update slider component

--- src/components/Slider/SliderComponent.vue
+++ src/components/Slider/SliderComponent.vue
@@ -1,12 +1,13 @@
 <script setup>
-import { dummyData } from '@/assets/data/dummyData'
+import { dummyData } from '@/data/dummyData'
 import './slider-component.css'
 </script>
 
 <template>
   <div class="slider">
     <div class="slider-item" :key="item.id" v-for="item in dummyData">
-      {{ item.id }}
+      <h1 class="slider-item-text">{{ item.text }}</h1>
+      <img :src="item.img" alt="" class="slider-item-background-image" />
     </div>
   </div>
 </template>
