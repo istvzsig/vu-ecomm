99fdbb923ff5ec6b937b3b6b6d63ca22ecc2f375
istvzsig <istvanzsigmond@aol.com>
2024-06-22 11:14:42 +0200

    init

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { RouterView } from 'vue-router'
+import NavbarComponent from './components/Navbar/NavbarComponent.vue'
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <NavbarComponent />
+    </div>
+  </header>
+
+  <RouterView />
+</template>
---
+++ src/components/Navbar/NavbarComponent.vue
@@ -0,0 +1,9 @@
+<script setup>
+import { RouterLink } from 'vue-router'
+</script>
+<template>
+  <nav>
+    <RouterLink to="/">Home</RouterLink>
+    <RouterLink to="/contact">Contact</RouterLink>
+  </nav>
+</template>
