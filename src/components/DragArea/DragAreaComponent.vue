5ec5925d1980ce18c4c7dcd45ef987c55224b701
istvzsig <istvanzsigmond@aol.com>
2024-06-23 18:30:59 +0200

    update drag area

--- src/components/DragArea/DragAreaComponent.vue
+++ src/components/DragArea/DragAreaComponent.vue
@@ -1,16 +1,18 @@
 <script setup>
 import { ref } from 'vue'
 import { storeData } from '@/data/storeData'
+import { useCartStore } from '@/stores/cart'
 import './drag-area.css'
 
-const isDragged = ref(false)
+const randomItem = ref(null)
+const cart = useCartStore()
 
 function getRandomItem() {
-  return storeData[(Math.random() * storeData.length) | 0]
+  randomItem.value = storeData[(Math.random() * storeData.length) | 0]
 }
 
 function toggleIsDragged() {
-  isDragged.value = !isDragged.value
+  // isDragged.value = !isDragged.value
 }
 
 function handleOndrag(event) {
@@ -20,10 +22,20 @@ function handleOndrag(event) {
 
 function handleDragStart(event) {
   // toggleIsDragged()
-  console.log(isDragged.value)
+  event.target.draggable = true
+  // event.target.style.backgroundColor = 'red'
+  console.log(event.target)
+  // console.log(isDragged.value)
 }
 
-const randomItem = getRandomItem()
+function handleAddItemToCart(event) {
+  event.stopPropagation()
+  cart.addItem(randomItem.value)
+}
+
+function handleOnClick(event) {}
+
+getRandomItem()
 </script>
 <template>
   <div class="drag-area">
@@ -32,20 +44,20 @@ const randomItem = getRandomItem()
       <div class="drag-area-sides-side"></div>
     </div>
 
-    <div @click="handleOnClick" @dragstart="handleDragStart" class="drag-area-item-card">
+    <div @dragstart="handleDragStart" class="drag-area-item-card">
       <h3 class="random-item-type-badge">{{ randomItem.type }}</h3>
-      <!-- {{ getRandomItem().img }} -->
       <img :src="randomItem.img" alt="" />
       <h1 class="random-item-name">{{ randomItem.name }}</h1>
-      <h3 class="random-item-text">{{ [...randomItem.text].splice(0, 100).join('') }}</h3>
+      <h3 class="random-item-text">{{ randomItem.text }}</h3>
       <h1 class="random-item-price">
-        {{ randomItem.price }}
+        ${{ randomItem.price }}
         <span class="random-item-price-currency">{{
           randomItem.price === 'FREE' ? '' : 'USD'
         }}</span>
       </h1>
-      <button class="random-item-add-to-cart-button">ADD TO CART</button>
-      <!-- {{ randomItem }} -->
+      <button @click="handleAddItemToCart" class="random-item-add-to-cart-button">
+        ADD TO CART
+      </button>
     </div>
   </div>
 </template>
