07bddaa90c22da942d25b61a800f9e4aa4afe4aa
istvzsig <istvanzsigmond@aol.com>
2024-06-23 09:53:58 +0200

    refactor slider

--- src/components/Slider/SliderComponent.vue
+++ src/components/Slider/SliderComponent.vue
@@ -1,27 +1,27 @@
 <script setup>
 import { dummyData } from '@/data/dummyData'
 import { useSliderStore } from '@/stores/slider'
-import './slider-component.css'
-const sliderStore = useSliderStore()
-sliderStore.startAnimate()
+import './slider.css'
+const slider = useSliderStore()
+slider.startAnimate()
 
-const sliderTexts = ['Race', 'From', 'Another', 'Space']
+const texts = ['Race', 'From', 'Another', 'Space']
 
-function sliderAnimation() {
+function animate() {
   return setTimeout(() => {
-    sliderStore.nextIndex = ++sliderStore.value % dummyData.length
-    sliderStore.stopAnimate()
-    sliderStore.text = sliderTexts[sliderStore.nextIndex].text
-    setTimeout(sliderAnimation, 4200)
+    slider.nextIndex = ++slider.value % dummyData.length
+    slider.stopAnimate()
+    slider.text = texts[slider.nextIndex].text
+    setTimeout(animate, 4200)
   }, 4200)
 }
-sliderAnimation()
+animate()
 </script>
 
 <template>
   <div class="slider">
-    <h1 class="slider-item-text">{{ dummyData[sliderStore.nextIndex].text }}</h1>
-    <img :src="dummyData[sliderStore.nextIndex].img" class="slider-background-image" />
+    <h1 class="slider-item-text">{{ dummyData[slider.nextIndex].text }}</h1>
+    <img :src="dummyData[slider.nextIndex].img" class="slider-background-image" />
   </div>
 </template>
 
