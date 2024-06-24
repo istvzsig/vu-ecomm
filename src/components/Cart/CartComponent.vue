34969f61447b5f8fd59f899af50002c7791054d9
istvzsig <istvanzsigmond@aol.com>
2024-06-24 22:50:21 +0200

    update cart css

--- src/components/Cart/CartComponent.vue
+++ src/components/Cart/CartComponent.vue
@@ -6,6 +6,10 @@ const cart = useCartStore()
 function renderTotalAmount() {
   return cart.totalAmount > 0 ? `Total amount: ${cart.totalAmount}` : 'Empty cart'
 }
+
+function renderText(text) {
+  return [...text].splice(0, 55).join('') + '...'
+}
 </script>
 
 <template>
@@ -15,9 +19,12 @@ function renderTotalAmount() {
     </h1>
     <div v-for="item in cart.items" :key="item.index">
       <div class="cart-item">
+        <div class="cart-item-img">
+          <img :src="item.img" />
+        </div>
         <div class="cart-item-details">
           <p class="cart-item-name">{{ item.name }}</p>
-          <p class="cart-item-description">{{ item.text }}</p>
+          <p class="cart-item-description">{{ renderText(item.text) }}</p>
         </div>
         <div @click="() => cart.deleteItem(item)" class="cart-item-delete-button">
           <svg
@@ -27,6 +34,7 @@ function renderTotalAmount() {
             width="100"
             height="100"
             viewBox="0 0 24 24"
+            fill="white"
           >
             <path
               d="M 10.806641 2 C 10.289641 2 9.7956875 2.2043125 9.4296875 2.5703125 L 9 3 L 4 3 A 1.0001 1.0001 0 1 0 4 5 L 20 5 A 1.0001 1.0001 0 1 0 20 3 L 15 3 L 14.570312 2.5703125 C 14.205312 2.2043125 13.710359 2 13.193359 2 L 10.806641 2 z M 4.3652344 7 L 5.8925781 20.263672 C 6.0245781 21.253672 6.877 22 7.875 22 L 16.123047 22 C 17.121047 22 17.974422 21.254859 18.107422 20.255859 L 19.634766 7 L 4.3652344 7 z"
