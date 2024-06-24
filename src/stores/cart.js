09cafdc21bb0cd805ef9966e87953d669b493688
istvzsig <istvanzsigmond@aol.com>
2024-06-24 22:37:05 +0200

    update cart store and component with delete

--- src/stores/cart.js
+++ src/stores/cart.js
@@ -2,7 +2,7 @@ import { defineStore } from 'pinia';
 
 export const useCartStore = defineStore('slider', {
   state: function () {
-    const store = JSON.parse(localStorage.getItem('cart'));
+    const store = JSON.parse(window.sessionStorage.getItem('cart'));
     return {
       items: store ? store : [],
       totalAmount: store ? store.length : 0,
@@ -11,12 +11,17 @@ export const useCartStore = defineStore('slider', {
   },
   actions: {
     addItem(item) {
-      this.items.push(item);
+      this.items.push({ ...item, index: this.items.length });
       this.totalAmount++;
       this.save();
     },
+    deleteItem(item) {
+      this.items = this.items.filter(_item => _item.index !== item.index);
+      this.totalAmount--;
+      this.save();
+    },
     save() {
-      window.localStorage.setItem("cart", JSON.stringify(this.items));
+      window.sessionStorage.setItem("cart", JSON.stringify(this.items));
     },
   },
 })
